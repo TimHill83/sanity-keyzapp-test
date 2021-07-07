f37c7297e2aa4e0db9dc88750703ab0e646353b2
Tim Hill <tim.hill@keyzapp.com>
2021-07-07 18:59:38 +0100

    Add h4

--- schemas/emailContent.js
+++ schemas/emailContent.js
@@ -16,6 +16,7 @@ export default {
             { title: "Heading 1", value: "h1" },
             { title: "Heading 2", value: "h2" },
             { title: "Heading 3", value: "h3" },
+            { title: "Heading 4", value: "h4" },
             { title: "Small", value: "small" },
             { title: "Small - Centred", value: "small-centred" },
           ],
