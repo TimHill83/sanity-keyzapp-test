5f8cb07725135557ec26080d100014fe24ebb438
Tim Hill <tim.hill@keyzapp.com>
2021-07-15 08:27:06 +0100

    Add alt text to Rich Image

--- schemas/richImage.js
+++ schemas/richImage.js
@@ -9,6 +9,18 @@ export default {
       title: "Target Width",
       description: "Specify a target output width for the image",
       type: "number",
+      options: {
+        isHighlighted: true,
+      },
+    },
+    {
+      name: "altText",
+      title: "Alternative Text",
+      description: "Text describing the image if it is not viewable",
+      type: "string",
+      options: {
+        isHighlighted: true,
+      },
     },
   ],
 };
