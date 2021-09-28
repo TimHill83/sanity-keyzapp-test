d3c1a73e807aed4882096ed615610b3db9c2846d
Tim Hill <tim.hill@keyzapp.com>
2021-09-28 20:04:37 +0100

    Update style handling

--- components/mjml/EmailContent.js
+++ components/mjml/EmailContent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import BlockContent from "@sanity/block-content-to-react";
+import RichImageBlock from "./RichImageBlock";
 
 //this component stops the default block rendering behaviour of wrapping everything in a div, by using a Fragment instead
 const NoWrapper = (props) => <>{props.children}</>;
@@ -7,21 +8,28 @@ const NotFound = (props) => <mj-text color="red">Component not found: {props.nod
 
 const BlockRenderer = (props) => {
   const { style = "normal" } = props.node; // not entirely sure what this does.
+  // Style handing examples
+  // //how to render the normal style
+  // if (style === "normal") {
+  //   return <mj-text mj-class="normal">{props.children}</mj-text>;
+  // }
 
-  //how to render the normal style
-  if (style === "normal") {
-    return <mj-text mj-class="normal">{props.children}</mj-text>;
-  }
+  // //how to render headings
+  // if (/^h\d/.test(style)) {
+  //   const level = style.replace(/[^\d]/g, "");
+  //   return <mj-text mj-class={`h${level}`}>{props.children}</mj-text>;
+  // }
 
-  //how to render headings
-  if (/^h\d/.test(style)) {
-    const level = style.replace(/[^\d]/g, "");
-    return <mj-text mj-class={`h${level}`}>{props.children}</mj-text>;
-  }
+  // //how to render the small style
+  // if (style === "small") {
+  //   return <mj-text mj-class="small">{props.children}</mj-text>;
+  // }
 
   // if (style === "blockquote") {
   //   return <blockquote>- {props.children}</blockquote>;
   // }
+
+  return <mj-text mj-class={style}>{props.children}</mj-text>;
 
   // Fall back to default handling
   return BlockContent.defaultSerializers.types.block(props);
@@ -32,7 +40,14 @@ function EmailContent(props) {
   return (
     <BlockContent
       blocks={content.content}
-      serializers={{ types: { block: BlockRenderer }, container: NoWrapper, unknownType: NotFound }}
+      serializers={{
+        types: {
+          block: BlockRenderer,
+          richImage: RichImageBlock,
+        },
+        container: NoWrapper,
+        unknownType: NotFound,
+      }}
     ></BlockContent>
   );
 }
